c6bd3d21fc05cfb8aaca11600f146e70b3298f08
Aaron <chihyenhsiao>
2016-09-25 14:15:35 +0800

    方法一 直接自訂函數來處理 Button預設之onClick Event

--- app/src/main/java/com/example/aaron/android_pratice_button_click/MainActivity.java
+++ app/src/main/java/com/example/aaron/android_pratice_button_click/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.aaron.android_pratice_button_click;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_Show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_Show = (TextView) findViewById(R.id.tv_Show);
+    }
+
+    public void btn_Click(View view)
+    {
+        int id = view.getId();
+
+        if(id == R.id.btn1)
+        {
+            tv_Show.setText("我是Button One");
+        }
+
+        if(id == R.id.btn2)
+        {
+            tv_Show.setText("我是Button Two");
+        }
     }
 }
